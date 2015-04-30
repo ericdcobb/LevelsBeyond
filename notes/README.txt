8e513323f40e2e0574ffe0c318bf450419870614
Jason H <rockchalkjay@gmail.com>
2015-04-30 00:22:17 -0600

    v1

---
+++ notes/README.txt
@@ -0,0 +1,50 @@
+READ ME
+———————————————
+
+This readme contains software requirements, install directions and quiz answers for Levels Beyond’s technical interview. 
+
+Requirements
+———————————————
+- JDK 1.7 or later
+- Maven 3.0+
+
+
+
+How to install and run
+———————————————
+
+1) Get the project: 
+
+git clone https://github.com/RockChalkJay/LevelsBeyond-NotesAPI.git
+
+2) Build the notes jar file with Maven: 
+
+mvm clean package
+
+3) Run the application with this command. The application by default uses port 8080. Optionally, you can bind the application to another port using the --server.port parameter. 
+
+java -jar ./target/notes-0.1.0.jar --server.port=8082
+
+———————————————
+How well does your note-searching-api scale or not scale? How would you make your search more efficient?
+
+My search is not scalable in that it is a brute force method. Performance is measured in two ways: CPU cycles and memory usage. I’m going to assume we are aiming for speed. In that case, I’d create a binary search with the keywords (meaning all unique words in each note)being the search term and the nodes containing all the notes containing that keyword.
+
+
+How would you add security to your API?
+
+Spring, conveniently, has a security module package. This package handles authentication.
+However, the login and password (along all other data) will be in clear text. Encryption can be implemented via https instead of http.
+
+What features should we add to this API next?
+
+1) Remove a note
+2) Edit an existing note
+3) Sort notes by date
+4) Make notes persistent e.g. backed by a database
+5) Add categories
+6) Unique notebooks per user
+
+How would you test the API?
+
+First, I would create JUnit tests. These set of test cases would perform checks of the functionality of Note.java and NoteBook.java classes. Next I would use a tool like SoapUI (now known as Ready!API) to test end to end functionality along with load testing.
--- notes/src/main/java/com/levelsbeyond/notes/NoteBook.java
+++ notes/src/main/java/com/levelsbeyond/notes/NoteBook.java
@@ -4,7 +4,14 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ * Class to collect and manage notes
+ * @author Jason Harris
+ *
+ */
 public class NoteBook {
 	//TODO: Synchronize
 	private Hashtable<Long,Note> noteList;
@@ -14,29 +21,61 @@ public class NoteBook {
 		noteList = new Hashtable<Long,Note>();
 	}
 	
+	/**
+	 * Returns a singleton instance of NoteBook
+	 * @return
+	 */
 	public static NoteBook getInstance( ) {
 	      return instance;
 	}
 	
+	/**
+	 * This class adds a new Note to the collection. Duplicate Ids and 
+	 * content are allowed. However, if an Id exists, the new Notes content will
+	 * overwrite the existing content
+	 * @param newNote The note to be added to the note book
+	 */
 	public void addNote(Note newNote) {
 		noteList.put(new Long(newNote.getId()), newNote);
 	}
 	
+	/**
+	 * @param id the Note to be retrieved
+	 * @return The Note with the given Id. If no Note exists with that Id, null is returned
+	 */
 	public Note getNote(long id) {
 		return noteList.get(new Long(id));
 	}
 	
+	/**
+	 * @return All Notes in storage 
+	 */
 	public Collection<Note> getAllNotes() {
 		return noteList.values();
 	}
 	
+	/**
+	 * Searches all stored Notes and returns those containing at least one. 
+	 * instance of keyword
+	 * @param keyword search term
+	 * @return Notes containing at least one instance of keyword. If there are no
+	 * matches an empty ArrayList is returned
+	 */
 	public ArrayList<Note> find(String keyword)
 	{
 		//TODO: Refactor
 		ArrayList<Note> matches = new ArrayList<Note>();
+		
+		String patternString = "\\b(" + keyword + ")\\b";
+		Pattern pattern = Pattern.compile(patternString);
+		Matcher matcher;
+		
 		for (Enumeration<Note> e = noteList.elements(); e.hasMoreElements();) {
 			Note nextNote = e.nextElement();
-			if (nextNote.getContent().indexOf(keyword) != -1)
+			
+			matcher = pattern.matcher(nextNote.getContent());
+			
+			if (matcher.find())
 			{
 				matches.add(nextNote);
 			}
